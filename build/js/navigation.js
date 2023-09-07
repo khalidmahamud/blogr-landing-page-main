62d5247e94366cb092a04af29e3a7aafcbcaa19b
Khalid <22-46601-1@student.aiub.edu>
2023-09-07 23:50:36 +0600

    completed navtop

--- build/js/navigation.js
+++ build/js/navigation.js
@@ -1,8 +1,19 @@
 const menuToggleBtn = document.querySelector('.menu-toggle-btn');
 const hamburgerIcon = document.querySelector('#hamburger-icon');
 const closeIcon = document.querySelector('#close-icon');
+const navLink = document.querySelectorAll('.navlinks')
 
 menuToggleBtn.addEventListener('click', () => {
     hamburgerIcon.classList.toggle('hidden');
     closeIcon.classList.toggle('hidden');
+});
+
+navLink.forEach(link => {
+    link.addEventListener('click', () => {
+        const parent = link.parentElement;
+        const navLinkMenu = parent.querySelector('ul');
+
+        navLinkMenu.classList.toggle('navlink-active');
+        navLinkMenu.classList.toggle('h-0');
+    });
 });
